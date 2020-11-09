112fdf5a2590a1ebe71a5dbe93cb9735a850d918
Blythe Dorrian <70557483+bdorrian16@users.noreply.github.com>
2020-11-08 20:37:36 -0600

    Updates continued

--- final.js
+++ final.js
@@ -47,3 +47,52 @@ randomButton.addEventListener('click', function() {
   photoDivs[currentPhotoNumber].classList.remove('hideThisDiv');
 
 });
+
+var chartDiv = document.getElementById('chart_container');
+
+var data = [{
+        "Name": "Joe Biden",
+        "Value": 2739
+    },
+    {
+        "Name": "Bernie Sanders",
+        "Value": 1119
+    },
+    {
+        "Name": "Elizabeth Warren",
+        "Value": 53
+    },
+    {
+        "Name": "Mike Bloomberg",
+        "Value": 46
+    },
+    {
+        "Name": "Pete Buttigieg",
+        "Value": 15
+    },
+    {
+        "Name": "Amy Klobuchar",
+        "Value": 5
+    },
+    {
+        "Name": "Tulsi Gabbard",
+        "Value": 2
+    }
+];
+
+var arrayValues = [];
+
+for (var i = 0; i < data.length; i++) {
+    arrayValues.push(data[i].Value);
+};
+
+var largest = Math.max.apply(Math, arrayValues);
+
+for (var i = 0; i < data.length; i++) {
+
+    chartDiv.innerHTML += '<div class="row" id="r' + i + '">' +
+        '<div class="Name">' + data[i].Name + '</div>' +
+        '<div class="Value" style="width:' + (data[i].Value * 58) / largest + '%"></div>' +
+        '<div class="ValueNumber">' + data[i].Value + '</div>' +
+        '</div>'
+}
